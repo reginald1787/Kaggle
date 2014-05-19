e7a3b5263e7388c53fe9a82132dc1f66698f3a63
Li <reginald1787@gmail.com>
2014-05-19 13:08:57 +0200

    Update gen_feature.py

--- gen_feature.py
+++ gen_feature.py
@@ -16,6 +16,7 @@ use "repeatertime" to duplicate samples as to enchance loyal customes' weights.
 
 from datetime import datetime, date
 from collections import defaultdict
+import numpy as np
 
 loc_offers = "offers.csv"
 loc_transactions = "transactions.csv"
@@ -87,6 +88,7 @@ def generate_features(loc_train, loc_test, loc_transactions, loc_out_train, loc_
 		#iterate through reduced dataset 
 		last_id = 0
 		features = defaultdict(float)
+		#chain = {}
 		for e, line in enumerate( open(loc_transactions) ):
 			if e > 0: #skip header
 				#poor man's csv reader
@@ -113,6 +115,11 @@ def generate_features(loc_train, loc_test, loc_transactions, loc_out_train, loc_
 					if "has_bought_brand" in features and "has_bought_company" in features:
 						features['has_bought_brand_company'] = 1
 						
+					if "chain_visited" not in features:
+						features['never_visited_chain'] = 1
+
+					#features['max_chain_visited_portion'] = np.array(chain.values())/float(sum(chain.values()))).max()
+
 					outline = ""
 					test = False
 					for k, v in features.items():
@@ -132,7 +139,9 @@ def generate_features(loc_train, loc_test, loc_transactions, loc_out_train, loc_
 						out_train.write( outline )
 					#print "Writing features or storing them in an array"
 					#reset features
+					#reset chain
 					features = defaultdict(float)
+					#chain = {}
 				#generate features from transaction record
 				#check if we have a test sample or train sample
 				if row[0] in train_ids or row[0] in test_ids:
@@ -153,84 +162,111 @@ def generate_features(loc_train, loc_test, loc_transactions, loc_out_train, loc_
 					#print "offers", offers[ train_ids[row[0]][2] ]
 					#print
 
-					################## my modification
-					features['chain'] = history[1]
-					features['market'] = history[3]
-					################## add chain information
 
+					# features: offer_value, offer_quantity, total_spend are temporarilly disabled 
+					# features: chain_visited is added
 
-					features['offer_value'] = offers[ history[2] ][4]
-					features['offer_quantity'] = offers[ history[2] ][2]
+					#chain = {}
+					#market = {}
+
+					# if row[1] in chain.keys():
+					# 	chain[row[1]]+=1
+					# else:
+					# 	chain[row[1]]=1
+
+					if history[1] == row[1]:
+						features['chain_visited'] += 1.0
+						date_diff_days = diff_days(row[6],history[-1])
+						if date_diff_days < 30:
+							features['chain_visited_30'] += 1.0
+							
+						if date_diff_days < 60:
+							features['chain_visited_60'] += 1.0
+							
+						if date_diff_days < 90:
+							features['chain_visited_90'] += 1.0
+							
+						if date_diff_days < 180:
+							features['chain_visited_180'] += 1.0
+							
+
+					#features['offer_value'] = offers[ history[2] ][4]
+					#features['offer_quantity'] = offers[ history[2] ][2]
 					offervalue = offers[ history[2] ][4]
 					
-					features['total_spend'] += float( row[10] )
+					#features['total_spend'] += float( row[10] )
+
+					# drop quantity and amount feature
 					
 					if offers[ history[2] ][3] == row[4]:
 						features['has_bought_company'] += 1.0
-						features['has_bought_company_q'] += float( row[9] )
-						features['has_bought_company_a'] += float( row[10] )
-						
+						#features['has_bought_company_q'] += float( row[9] )
+						#features['has_bought_company_a'] += float( row[10] )
 						date_diff_days = diff_days(row[6],history[-1])
 						if date_diff_days < 30:
 							features['has_bought_company_30'] += 1.0
-							features['has_bought_company_q_30'] += float( row[9] )
-							features['has_bought_company_a_30'] += float( row[10] )
+							#features['has_bought_company_q_30'] += float( row[9] )
+							#features['has_bought_company_a_30'] += float( row[10] )
 						if date_diff_days < 60:
 							features['has_bought_company_60'] += 1.0
-							features['has_bought_company_q_60'] += float( row[9] )
-							features['has_bought_company_a_60'] += float( row[10] )
+							#features['has_bought_company_q_60'] += float( row[9] )
+							#features['has_bought_company_a_60'] += float( row[10] )
 						if date_diff_days < 90:
 							features['has_bought_company_90'] += 1.0
-							features['has_bought_company_q_90'] += float( row[9] )
-							features['has_bought_company_a_90'] += float( row[10] )
+							#features['has_bought_company_q_90'] += float( row[9] )
+							#features['has_bought_company_a_90'] += float( row[10] )
 						if date_diff_days < 180:
 							features['has_bought_company_180'] += 1.0
-							features['has_bought_company_q_180'] += float( row[9] )
-							features['has_bought_company_a_180'] += float( row[10] )
+							#features['has_bought_company_q_180'] += float( row[9] )
+							#features['has_bought_company_a_180'] += float( row[10] )
 					
-					if offers[ history[2] ][1] == row[3]:
-						
+					if offers[ history[2] ][1] == row[3]:	
 						features['has_bought_category'] += 1.0
-						features['has_bought_category_q'] += float( row[9] )
-						features['has_bought_category_a'] += float( row[10] )
+						#features['has_bought_category_q'] += float( row[9] )
+						#features['has_bought_category_a'] += float( row[10] )
 						date_diff_days = diff_days(row[6],history[-1])
 						if date_diff_days < 30:
 							features['has_bought_category_30'] += 1.0
-							features['has_bought_category_q_30'] += float( row[9] )
-							features['has_bought_category_a_30'] += float( row[10] )
+							#features['has_bought_category_q_30'] += float( row[9] )
+							#features['has_bought_category_a_30'] += float( row[10] )
 						if date_diff_days < 60:
 							features['has_bought_category_60'] += 1.0
-							features['has_bought_category_q_60'] += float( row[9] )
-							features['has_bought_category_a_60'] += float( row[10] )
+							#features['has_bought_category_q_60'] += float( row[9] )
+							#features['has_bought_category_a_60'] += float( row[10] )
 						if date_diff_days < 90:
 							features['has_bought_category_90'] += 1.0
-							features['has_bought_category_q_90'] += float( row[9] )
-							features['has_bought_category_a_90'] += float( row[10] )						
+							#features['has_bought_category_q_90'] += float( row[9] )
+							#features['has_bought_category_a_90'] += float( row[10] )						
 						if date_diff_days < 180:
 							features['has_bought_category_180'] += 1.0
-							features['has_bought_category_q_180'] += float( row[9] )
-							features['has_bought_category_a_180'] += float( row[10] )				
+							#features['has_bought_category_q_180'] += float( row[9] )
+							#features['has_bought_category_a_180'] += float( row[10] )		
+
 					if offers[ history[2] ][5] == row[5]:
 						features['has_bought_brand'] += 1.0
-						features['has_bought_brand_q'] += float( row[9] )
-						features['has_bought_brand_a'] += float( row[10] )
+						#features['has_bought_brand_q'] += float( row[9] )
+						#features['has_bought_brand_a'] += float( row[10] )
 						date_diff_days = diff_days(row[6],history[-1])
 						if date_diff_days < 30:
 							features['has_bought_brand_30'] += 1.0
-							features['has_bought_brand_q_30'] += float( row[9] )
-							features['has_bought_brand_a_30'] += float( row[10] )
+							#features['has_bought_brand_q_30'] += float( row[9] )
+							#features['has_bought_brand_a_30'] += float( row[10] )
 						if date_diff_days < 60:
 							features['has_bought_brand_60'] += 1.0
-							features['has_bought_brand_q_60'] += float( row[9] )
-							features['has_bought_brand_a_60'] += float( row[10] )
+							#features['has_bought_brand_q_60'] += float( row[9] )
+							#features['has_bought_brand_a_60'] += float( row[10] )
 						if date_diff_days < 90:
 							features['has_bought_brand_90'] += 1.0
-							features['has_bought_brand_q_90'] += float( row[9] )
-							features['has_bought_brand_a_90'] += float( row[10] )						
+							#features['has_bought_brand_q_90'] += float( row[9] )
+							#features['has_bought_brand_a_90'] += float( row[10] )						
 						if date_diff_days < 180:
 							features['has_bought_brand_180'] += 1.0
-							features['has_bought_brand_q_180'] += float( row[9] )
-							features['has_bought_brand_a_180'] += float( row[10] )	
+							#features['has_bought_brand_q_180'] += float( row[9] )
+							#features['has_bought_brand_a_180'] += float( row[10] )	
+
+					# add geography features: max_chain_visited_portion, max_market visited_portion
+
+
 				last_id = row[0]
 				if e % 100000 == 0:
 					print e
